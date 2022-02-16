ecd468b67091eaa640dc28d982ae1b1d59927637
Jawoon Kim <jawoon.kim@edu.bib.de>
2022-02-16 16:48:28 +0100

    tailwindcss init

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="App">
-      <h2>Init</h2>
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <h2 className="text-4xl font-semibold">Tetris Battle Royale</h2>
     </div>
   );
 }
