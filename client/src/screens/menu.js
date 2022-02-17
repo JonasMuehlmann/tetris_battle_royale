c613572c57f1c71c49e4aab8b8e7c8700835ea4a
Jawoon Kim <jawoon.kim@edu.bib.de>
2022-02-17 08:55:06 +0100

    navigation in menu

--- client/src/screens/menu.js
+++ client/src/screens/menu.js
@@ -1,4 +1,20 @@
+import { useContext } from "react";
+import { Screen, ScreenContext } from "../contexts/screen-context";
+
+const styles = {
+  menuItem: 'text-2xl font-semibold text-left w-1/2 pl-14',
+  logOutButton: 'text-md opacity-50 text-left'
+}
+
 const Menu = () => {
+  const { navigate } = useContext(ScreenContext)
+
+  const items = [
+    { text: 'Matchfinder', onClick: e => navigate(Screen.Queue) },
+    { text: 'Profile', onClick: e => navigate(Screen.Profile) },
+    { text: 'Settings', onClick: e => navigate(Screen.Settings) },
+  ]
+
   return (
     <div className="w-full h-full flex p-24">
       <div className="h-full flex flex-col">
@@ -9,17 +25,20 @@ const Menu = () => {
           Main Menu
         </h3>
         <ul className="flex flex-col">
-          <li>
-            Matchfinder
-          </li>
-          <li>
-            Profile
-          </li>
-          <li>
-            Settings
-          </li>
+          {
+            items.map((t, i) => (
+              <li
+                key={i}
+                onClick={t.onClick}
+                className={styles.menuItem}>
+                {t.text}
+              </li>
+            ))
+          }
         </ul>
-        <button className="text-md opacity-50 text-left">
+        <button
+          onClick={e => navigate(Screen.LogIn)}
+          className={styles.logOutButton}>
           Log out
         </button>
       </div>
