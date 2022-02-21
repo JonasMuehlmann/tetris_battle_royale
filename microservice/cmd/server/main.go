89dd94769354536fd7ea24458ad56bc5963a61de
jonas <jonasmuehlmann@protonmail.com>
2022-02-21 10:23:59 +0100

    refactor(api): integrate user service into main

--- microservice/cmd/server/main.go
+++ microservice/cmd/server/main.go
@@ -4,24 +4,22 @@ import (
 	"log"
 	userService "microservice/api/services/user_service"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	// Forwards requests to it's registered handlers
 	// by matching the endpoint (e.g. "/") to the handler
 	// This is the gateway in the microservice diagram
-	mux := http.NewServeMux()
-	userServiceMux := userService.MakeUserServiceMux()
+	mux := mux.NewRouter()
 
 	// TODO: The routers can be simplified with gorilla/mux
-	mux.Handle("/user/", http.StripPrefix("/user", userServiceMux))
 	mux.Handle("/", http.FileServer(http.Dir("../client/build/")))
+	mux.HandleFunc("/login", userService.LoginHandler)
+	mux.HandleFunc("/isLogin", userService.IsLoginHandler)
+	mux.HandleFunc("/logout", userService.LogoutHandler)
 
 	log.Println("Starting server on Port 8080")
-
-	err := http.ListenAndServe(":8080", mux)
-
-	if err != nil {
-		log.Fatalf("server failed to start: %v", err)
-	}
+	log.Fatalf("server failed to start: %v", http.ListenAndServe(":8080", mux))
 }
