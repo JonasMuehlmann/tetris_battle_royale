531036f8ba35cd64f956b763808d1121fae6c5e1
jonas <jonasmuehlmann@protonmail.com>
2022-02-22 16:15:23 +0100

    refactor: refactor `api/common/db.go` as a repository

--- microservice/internal/repository/postgres/session_repository.go
+++ microservice/internal/repository/postgres/session_repository.go
@@ -1,4 +1,71 @@
 package repository
 
-type SessionRepository struct {
+import (
+	"errors"
+	"log"
+	"microservice/api/model"
+	"time"
+)
+
+type PostgresDatabaseSessionRepository struct {
+	PostgresDatabase
+}
+
+func (repo *PostgresDatabaseUserRepository) CreateSession(userID int) (int, error) {
+
+	user, err := GetUserFromID(userID)
+	session := model.Session{ID: -1, UserID: user.ID, CreationTime: time.Now()}
+
+	db, err := repo.GetDBConnection()
+	if err != nil {
+		return 0, err
+	}
+
+	err = db.QueryRow("INSERT INTO sessions(user_ID, creation_time) VALUES($1, $2) RETURNING ID", session.UserID, session.CreationTime).Scan(&session.ID)
+	if err != nil {
+		log.Printf("Error: %v", err)
+
+		return 0, err
+	}
+
+	return session.ID, nil
+}
+
+func (repo *PostgresDatabaseUserRepository) GetSession(userID int) (model.Session, error) {
+	session := model.Session{}
+
+	db, err := repo.GetDBConnection()
+	if err != nil {
+		return session, err
+	}
+
+	err = db.Get(&session, "SELECT * FROM sessions WHERE user_ID = $1", userID)
+	if err != nil {
+		return model.Session{}, errors.New("Failed to retrieve session")
+	}
+
+	return session, nil
+}
+
+func (repo *PostgresDatabaseUserRepository) DeleteSession(sessionID int) error {
+	db, err := repo.GetDBConnection()
+	if err != nil {
+		return err
+	}
+
+	res, err := db.Exec("DELETE FROM sessions WHERE ID = $1", sessionID)
+	if err != nil {
+		return err
+	}
+
+	numDeletedSessions, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if numDeletedSessions == 0 {
+		return errors.New("Session does not exist")
+	}
+
+	return nil
 }
--- microservice/internal/repository/postgres/user_repository.go
+++ microservice/internal/repository/postgres/user_repository.go
@@ -1,7 +1,56 @@
-package adapters
+package repository
 
-type PostgresDatabaseRepository struct {
+import (
+	"microservice/api/model"
+)
+
+type PostgresDatabaseUserRepository struct {
+	PostgresDatabase
 }
 
-func (db *PostgresDatabaseRepository) RegisterUser(username, password, salt string) error {
+func (repo *PostgresDatabaseUserRepository) GetUserFromID(userID int) (model.User, error) {
+	user := model.User{}
+
+	db, err := repo.GetDBConnection()
+	if err != nil {
+		return model.User{}, nil
+	}
+
+	err = db.Get(&user, "SELECT * FROM users WHERE ID = $1", userID)
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+func (repo *PostgresDatabaseUserRepository) GetUserFromName(username string) (model.User, error) {
+	user := model.User{}
+
+	db, err := repo.GetDBConnection()
+	if err != nil {
+		return user, err
+	}
+
+	err = db.Get(&user, "SELECT * FROM users WHERE username = $1", username)
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+func (repo *PostgresDatabaseUserRepository) Register(username, password, salt string) (int, error) {
+	var userID int
+
+	db, err := repo.GetDBConnection()
+	if err != nil {
+		return 0, err
+	}
+	err = db.QueryRow("INSERT INTO users(username, pw_hash, salt) VALUES($1, $2, $3) RETURNING ID", username, string(password), string(salt)).Scan(&userID)
+	if err != nil {
+		return 0, err
+	}
+
+	return userID, nil
 }
