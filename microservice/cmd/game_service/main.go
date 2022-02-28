0c78ea3e61c50a00faffb63eb36f087bd5a7f52e
jonas <jonasmuehlmann@protonmail.com>
2022-02-28 10:13:22 +0100

    feat(api): implement main methods of game and matchmaking service

--- microservice/cmd/game_service/main.go
+++ microservice/cmd/game_service/main.go
@@ -1,12 +1,16 @@
 package main
 
-func main() {
-	// logger := log.New(os.Stdout, "TBR - ", log.Ltime|log.Lshortfile)
+import (
+	"log"
+	gameService "microservice/internal/core/services/game_service"
+	drivingAdapters "microservice/internal/driving_adapters/rest"
+	"os"
+)
 
-	// db := repository.MakeDefaultPostgresDB(logger)
-	// userRepository := repository.PostgresDatabaseUserRepository{Logger: logger, PostgresDatabase: *db}
-	// sessionRepository := repository.PostgresDatabaseSessionRepository{Logger: logger, PostgresDatabase: *db}
-	// userService := matchmakingService.UserService{Logger: logger, UserRepo: userRepository, SessionRepo: sessionRepository}
-	// userServiceAdapter := drivingAdapters.UserServiceRestAdapter{Logger: logger, Service: userService}
-	// userServiceAdapter.Run()
+func main() {
+	logger := log.New(os.Stdout, "TBR - ", log.Ltime|log.Lshortfile)
+
+	game_service := gameService.GameService{Logger: logger}
+	userServiceAdapter := drivingAdapters.GameServiceRestAdapter{Logger: logger, Service: game_service}
+	userServiceAdapter.Run()
 }
--- microservice/cmd/matchmaking_service/main.go
+++ microservice/cmd/matchmaking_service/main.go
@@ -1,12 +1,19 @@
 package main
 
-func main() {
-	// logger := log.New(os.Stdout, "TBR - ", log.Ltime|log.Lshortfile)
+import (
+	"log"
+	repository "microservice/internal/core/repository/postgres"
+	matchmakingService "microservice/internal/core/services/matchmaking_service"
+	drivingAdapters "microservice/internal/driving_adapters/rest"
+	"os"
+)
 
-	// db := repository.MakeDefaultPostgresDB(logger)
-	// userRepository := repository.PostgresDatabaseUserRepository{Logger: logger, PostgresDatabase: *db}
-	// sessionRepository := repository.PostgresDatabaseSessionRepository{Logger: logger, PostgresDatabase: *db}
-	// userService := matchmakingService.UserService{Logger: logger, UserRepo: userRepository, SessionRepo: sessionRepository}
-	// userServiceAdapter := drivingAdapters.UserServiceRestAdapter{Logger: logger, Service: userService}
-	// userServiceAdapter.Run()
+func main() {
+	logger := log.New(os.Stdout, "TBR - ", log.Ltime|log.Lshortfile)
+
+	db := repository.MakeDefaultPostgresDB(logger)
+	userRepository := repository.PostgresDatabaseUserRepository{Logger: logger, PostgresDatabase: *db}
+	matchmakingService := matchmakingService.MatchmakingService{Logger: logger, UserRepo: userRepository}
+	userServiceAdapter := drivingAdapters.MatchmakingServiceRestAdapter{Logger: logger, Service: matchmakingService}
+	userServiceAdapter.Run()
 }
