76a388f30af7fce67a0a52c6aa4a212f79292f63
jonas <jonasmuehlmann@protonmail.com>
2022-02-28 09:05:52 +0100

    feat(game_service): implement minimal game service logic needed for matchmaking

--- microservice/internal/core/services/game_service/game_service.go
+++ microservice/internal/core/services/game_service/game_service.go
@@ -1,13 +1,52 @@
-package matchmakingService
+package gameService
 
 import (
+	"context"
 	"log"
 	drivenPorts "microservice/internal/core/driven_ports"
+	gameServiceProto "microservice/internal/core/protofiles/game_service"
 	types "microservice/internal/core/types"
+
+	"google.golang.org/grpc"
 )
 
 type GameService struct {
-	UserRepo drivenPorts.UserPort
-	Logger   *log.Logger
-	Matches  map[int]types.Match
+	UserRepo   drivenPorts.UserPort
+	Logger     *log.Logger
+	Matches    map[int]types.Match
+	GrpcServer *grpc.Server
+}
+
+type GameServiceServer struct {
+	gameServiceProto.UnimplementedGameServiceServer
+	GameService GameService
+}
+
+func (service *GameServiceServer) StartGame(context context.Context, userIDList *gameServiceProto.UserIDList) (*gameServiceProto.MatchID, error) {
+	matchID, err := service.GameService.StartGame(userIDList.GetId())
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &gameServiceProto.MatchID{Id: int64(matchID)}, nil
+}
+
+func MakeGameService(userRepo drivenPorts.UserPort, logger *log.Logger) GameService {
+	grpcServer := grpc.NewServer()
+
+	gameService := GameService{
+		UserRepo:   userRepo,
+		Logger:     logger,
+		Matches:    make(map[int]types.Match),
+		GrpcServer: grpcServer,
+	}
+
+	gameServiceProto.RegisterGameServiceServer(grpcServer, &GameServiceServer{GameService: gameService})
+
+	return gameService
+}
+
+func (service *GameService) StartGame(userIDList []int64) (int, error) {
+	return 0, nil
 }
