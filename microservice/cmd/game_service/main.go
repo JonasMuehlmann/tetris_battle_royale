0e09c63b3e9de82f524a4b3dd03c8a23da764a14
jonas <jonasmuehlmann@protonmail.com>
2022-02-28 11:33:09 +0100

    fix(api): properly configure game service main method

--- microservice/cmd/game_service/main.go
+++ microservice/cmd/game_service/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	repository "microservice/internal/core/repository/postgres"
 	gameService "microservice/internal/core/services/game_service"
 	drivingAdapters "microservice/internal/driving_adapters/rest"
 	"os"
@@ -10,7 +11,9 @@ import (
 func main() {
 	logger := log.New(os.Stdout, "TBR - ", log.Ltime|log.Lshortfile)
 
-	game_service := gameService.GameService{Logger: logger}
+	db := repository.MakeDefaultPostgresDB(logger)
+	userRepo := repository.PostgresDatabaseUserRepository{Logger: logger, PostgresDatabase: *db}
+	game_service := gameService.MakeGameService(userRepo, logger)
 	userServiceAdapter := drivingAdapters.GameServiceRestAdapter{Logger: logger, Service: game_service}
 	userServiceAdapter.Run()
 }
