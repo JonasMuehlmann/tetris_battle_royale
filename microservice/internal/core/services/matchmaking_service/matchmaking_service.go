0ffa7579f9dd4786984c9e8e271e3925b041538f
jonas <jonasmuehlmann@protonmail.com>
2022-02-28 09:23:15 +0100

    feat(matchmaking_service): implement minimal logic to start game from matchmaking

--- microservice/internal/core/services/matchmaking_service/matchmaking_service.go
+++ microservice/internal/core/services/matchmaking_service/matchmaking_service.go
@@ -1,16 +1,40 @@
 package matchmakingService
 
 import (
+	"context"
 	"log"
 	drivenPorts "microservice/internal/core/driven_ports"
+	gameServiceProto "microservice/internal/core/protofiles/game_service"
+
+	"google.golang.org/grpc"
 )
 
 const MatchSize = 10
 
 type MatchmakingService struct {
-	UserRepo drivenPorts.UserPort
-	Logger   *log.Logger
-	Queue    map[int]bool
+	UserRepo              drivenPorts.UserPort
+	Logger                *log.Logger
+	Queue                 map[int]bool
+	GameServiceGrpcClient gameServiceProto.GameServiceClient
+}
+
+func MakeMatchmakingService(userRepo drivenPorts.UserPort, logger *log.Logger) (MatchmakingService, error) {
+	grpcConn, err := grpc.Dial("game-service:8081", grpc.WithInsecure())
+
+	if err != nil {
+		return MatchmakingService{}, err
+	}
+
+	gameServiceGrpcClient := gameServiceProto.NewGameServiceClient(grpcConn)
+
+	matchmakingService := MatchmakingService{
+		UserRepo:              userRepo,
+		Logger:                logger,
+		Queue:                 make(map[int]bool),
+		GameServiceGrpcClient: gameServiceGrpcClient,
+	}
+
+	return matchmakingService, nil
 }
 
 func (service MatchmakingService) Join(userID int) error {
@@ -33,6 +57,21 @@ func (service MatchmakingService) Leave(userID int) error {
 }
 
 func (service MatchmakingService) startGame() error {
-	// TODO: Hand off players to game service over grpc
+	userIDList := make([]int64, 0, len(service.Queue))
+
+	for k, v := range service.Queue {
+		if v {
+			userIDList = append(userIDList, int64(k))
+		}
+	}
+
+	_, err := service.GameServiceGrpcClient.StartGame(context.Background(), &gameServiceProto.UserIDList{Id: userIDList})
+
+	// TODO: Notify clients
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
