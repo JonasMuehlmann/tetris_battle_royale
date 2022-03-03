66884de1af072c7b3f6e82aabe880d637169eed5
jonas <jonasmuehlmann@protonmail.com>
2022-03-03 09:56:28 +0100

    chore: add a TODO comment

--- microservice/internal/driving_adapters/rest/user_service.go
+++ microservice/internal/driving_adapters/rest/user_service.go
@@ -115,10 +115,10 @@ func (adapter UserServiceRestAdapter) Run() {
 
 	mux.Handle("/", http.FileServer(http.Dir("../client/build/")))
 
-	// NOTE: The api gateay should contain a prefix user/, which is stripped before forwarding
 	mux.HandleFunc("/login", adapter.LoginHandler).Methods("POST")
 	mux.HandleFunc("/register", adapter.RegisterHandler).Methods("POST")
 	mux.HandleFunc("/isLogin/{username:[a-zA-Z0-9]+}", adapter.IsLoginHandler).Methods("GET")
+	// TODO: The id should be part of the URL, not the request body
 	mux.HandleFunc("/logout", adapter.LogoutHandler).Methods("DELETE")
 
 	adapter.Logger.Println("Starting server on Port 8080")
