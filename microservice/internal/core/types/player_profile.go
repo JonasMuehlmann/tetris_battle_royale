e74b0aceb8e892fad30e98338f8bf03643b3dc33
jonas <jonasmuehlmann@protonmail.com>
2022-03-04 23:20:57 +0100

    fix: add missing field for last update time

--- microservice/internal/core/types/player_profile.go
+++ microservice/internal/core/types/player_profile.go
@@ -1,9 +1,12 @@
 package types
 
+import "time"
+
 type PlayerProfile struct {
-	ID                 int    `db:"id" json:"id"`
-	UserID             string `db:"user_id" json:"user_id"`
-	Playtime           int    `db:"playtime" json:"playtime"`
-	PlayerRatingID     int    `db:"player_rating_id" json:"player_rating_id"`
-	PlayerStatisticsID int    `db:"player_statistics_id" json:"player_statistics_id"`
+	ID                 int       `db:"id" json:"id"`
+	UserID             string    `db:"user_id" json:"user_id"`
+	Playtime           int       `db:"playtime" json:"playtime"`
+	PlayerRatingID     int       `db:"player_rating_id" json:"player_rating_id"`
+	PlayerStatisticsID int       `db:"player_statistics_id" json:"player_statistics_id"`
+	LastUpdate         time.Time `json:"last_update" db:"last_update"`
 }
