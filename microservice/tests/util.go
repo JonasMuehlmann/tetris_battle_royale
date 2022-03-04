325aa73685265e635ca744fc799bc68ce66a98be
jonas <jonasmuehlmann@protonmail.com>
2022-03-04 16:57:49 +0100

    chore(tests): add helper methods to reset db after tests

---
+++ microservice/tests/util.go
@@ -0,0 +1,14 @@
+package util
+
+import (
+	"log"
+
+	"github.com/jmoiron/sqlx"
+)
+
+func ResetDB(db *sqlx.DB) {
+	_, err := db.Exec("TRUNCATE users, sessions, player_profiles, player_statistics,player_ratings, match_records CASCADE")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
