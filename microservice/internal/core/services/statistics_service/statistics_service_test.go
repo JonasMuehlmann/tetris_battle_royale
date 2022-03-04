d43f375ad378f0597f7d08c5a6d7407ee99b3177
jonas <jonasmuehlmann@protonmail.com>
2022-03-04 23:21:40 +0100

    tests: add tests `statisticsService.GetPlayerProfile()`

--- microservice/internal/core/services/statistics_service/statistics_service_test.go
+++ microservice/internal/core/services/statistics_service/statistics_service_test.go
@@ -2,9 +2,12 @@ package statisticsService_test
 
 import (
 	"log"
+	common "microservice/internal"
 	repository "microservice/internal/core/driven_adapters/repository/postgres"
 	statisticsService "microservice/internal/core/services/statistics_service"
+	"microservice/internal/core/types"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/suite"
 )
@@ -28,5 +31,35 @@ func TestRunStatisticsServiceTestSuite(t *testing.T) {
 }
 
 func (suite *statisticsServiceTestSuite) TestGetPlayerProfileBasic() {
-	suite.Equal(1, 2, "Oh no!")
+	db, err := suite.db.GetConnection()
+	suite.NoError(err)
+
+	common.ResetDB(db)
+
+	_, err = db.Exec("INSERT INTO users(id) VALUES('123e4567-e89b-12d3-a456-426614174000')")
+	suite.NoError(err)
+
+	_, err = db.Exec("INSERT INTO player_ratings(id) VALUES(0)")
+	suite.NoError(err)
+
+	_, err = db.Exec("INSERT INTO player_statistics(id) VALUES(0)")
+	suite.NoError(err)
+
+	_, err = db.Exec("INSERT INTO player_profiles VALUES(0, '123e4567-e89b-12d3-a456-426614174000', 0, 0, 0, '1999-01-08 04:05:06')")
+	suite.NoError(err)
+
+	playerProfile, err := suite.service.GetPlayerProfile("123e4567-e89b-12d3-a456-426614174000")
+	playerProfile.LastUpdate = playerProfile.LastUpdate.UTC()
+
+	date, err := time.Parse("2006-01-02 15:04:05", "1999-01-08 04:05:06")
+	suite.NoError(err)
+	suite.Equal(playerProfile, types.PlayerProfile{
+		ID:                 0,
+		UserID:             "123e4567-e89b-12d3-a456-426614174000",
+		Playtime:           0,
+		PlayerRatingID:     0,
+		PlayerStatisticsID: 0,
+		LastUpdate:         date.UTC(),
+	})
+	suite.NoError(err)
 }
