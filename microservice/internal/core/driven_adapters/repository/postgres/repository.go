4b29e45d392567131967d25c5974b538e625ea0b
jonas <jonasmuehlmann@protonmail.com>
2022-03-04 16:37:46 +0100

    chore: add function to create test db configuration

--- microservice/internal/core/driven_adapters/repository/postgres/repository.go
+++ microservice/internal/core/driven_adapters/repository/postgres/repository.go
@@ -60,6 +60,36 @@ func MakeDefaultPostgresDB(logger *log.Logger) *PostgresDatabase {
 		Logger:   logger,
 	}
 }
+func MakeDefaultPostgresTestDB(logger *log.Logger) *PostgresDatabase {
+	err := godotenv.Load(".postgres_credentials_test.env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	host := os.Getenv("TBR_PG_HOST")
+	portRaw := os.Getenv("TBR_PG_PORT")
+
+	var port int64
+
+	if portRaw == "" {
+		port = -1
+	} else {
+		port, err = strconv.ParseInt(portRaw, 10, 32)
+	}
+
+	username := os.Getenv("TBR_PG_USER")
+	dbName := os.Getenv("TBR_PG_DB")
+	password := os.Getenv("TBR_PG_PASSWORD")
+
+	return &PostgresDatabase{
+		Host:     host,
+		Port:     int(port),
+		Username: username,
+		Password: password,
+		DBName:   dbName,
+		Logger:   logger,
+	}
+}
 
 func (dbImpl *PostgresDatabase) MakeConnectionString() string {
 	connectionString := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable",
