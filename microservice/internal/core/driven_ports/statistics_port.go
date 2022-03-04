5183502c2e04f3052516d178c266d3d8229a882b
jonas <jonasmuehlmann@protonmail.com>
2022-03-04 12:12:02 +0100

    chore: define ports for statistics service

--- microservice/internal/core/driven_ports/statistics_port.go
+++ microservice/internal/core/driven_ports/statistics_port.go
@@ -1,4 +1,40 @@
 package drivenPorts
 
+import (
+	types "microservice/internal/core/types"
+	"time"
+)
+
 type StatisticsPort interface {
+	GetPlayerProfile(userID int) (types.PlayerProfile, error)
+	GetPlayerPlaytime(userID int) (int, error)
+	GetPlayerRating(userID int) (int, error)
+	GetPlayerProfileLastUpdateTime(userID int) (time.Time, error)
+
+	SetPlayerPlaytime(userID int) error
+	SetPlayerRating(rating int) error
+
+	GetPlayerStatistics(userID int) (types.PlayerStatistics, error)
+	GetPlayerScore(userID int) (int, error)
+	GetPlayerScorePerMinute(userID int) (float32, error)
+	GetPlayerWinrate(userID int) (float32, error)
+	GetPlayerNumLosses(userID int) (int, error)
+	GetPlayerNumWinsAsTop10(userID int) (int, error)
+	GetPlayerNumWinsAsTop5(userID int) (int, error)
+	GetPlayerNumWinsAsTop3(userID int) (int, error)
+	GetPlayerNumWinsAsTop1(userID int) (int, error)
+
+	SetPlayerScore(userID int) error
+	SetPlayerScorePerMinute(userID int) error
+	SetPlayerWinrate(userID int) error
+	SetPlayerNumLosses(userID int) error
+	SetPlayerNumWinsAsTop10(userID int) error
+	SetPlayerNumWinsAsTop5(userID int) error
+	SetPlayerNumWinsAsTop3(userID int) error
+	SetPlayerNumWinsAsTop1(userID int) error
+
+	GetPlayerMatchRecords(userID int) ([]types.MatchRecord, error)
+	GetMatchRecord(matchID int) (types.MatchRecord, error)
+
+	AddMatchRecord(userID int, record types.MatchRecord) error
 }
--- microservice/internal/core/driving_ports/statistics_service_port.go
+++ microservice/internal/core/driving_ports/statistics_service_port.go
@@ -1,4 +1,26 @@
 package drivingPorts
 
+import (
+	"microservice/internal/core/types"
+	"time"
+)
+
 type StatisticsServicePort interface {
+	GetPlayerProfile(userID int) (types.PlayerProfile, error)
+	GetPlayerPlaytime(userID int) (int, error)
+	GetPlayerRating(userID int) (int, error)
+	GetPlayerProfileLastUpdateTime(userID int) (time.Time, error)
+
+	GetPlayerStatistics(userID int) (types.PlayerStatistics, error)
+	GetPlayerScore(userID int) (int, error)
+	GetPlayerScorePerMinute(userID int) (float32, error)
+	GetWinrate(userID int) (float32, error)
+	GetNumLosses(userID int) (int, error)
+	GetNumWinsAsTop10(userID int) (int, error)
+	GetNumWinsAsTop5(userID int) (int, error)
+	GetNumWinsAsTop3(userID int) (int, error)
+	GetNumWinsAsTop1(userID int) (int, error)
+
+	GetPlayerMatchRecords(userID int) ([]types.MatchRecord, error)
+	GetMatchRecord(matchID int) (types.MatchRecord, error)
 }
