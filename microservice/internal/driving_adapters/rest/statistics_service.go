fd9f57f0e880281290cbad8124217f69141aeafa
jonas <jonasmuehlmann@protonmail.com>
2022-03-05 11:56:43 +0100

    feat: add API endpoint to retrieve player profile

--- microservice/internal/driving_adapters/rest/statistics_service.go
+++ microservice/internal/driving_adapters/rest/statistics_service.go
@@ -1,7 +1,9 @@
 package drivingAdapters
 
 import (
+	"encoding/json"
 	"log"
+	common "microservice/internal"
 	drivingPorts "microservice/internal/core/driving_ports"
 	"net/http"
 
@@ -13,13 +15,32 @@ type StatisticsServiceRestAdapter struct {
 	Logger  *log.Logger
 }
 
-func (adapter StatisticsServiceRestAdapter) GetHandler(w http.ResponseWriter, r *http.Request) {
+func (adapter StatisticsServiceRestAdapter) GetPlayerProfileHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	playerProfile, err := adapter.Service.GetPlayerProfile(vars["userID"])
+	if err != nil {
+		adapter.Logger.Printf("Error: %v", err)
+		common.TryWriteResponse(w, common.MakeJsonError(err.Error()))
+
+		return
+	}
+
+	marhshalleDplayerProfile, err := json.Marshal(playerProfile)
+	if err != nil {
+		adapter.Logger.Printf("Error: %v", err)
+		common.TryWriteResponse(w, common.MakeJsonError(err.Error()))
+
+		return
+	}
+
+	common.TryWriteResponse(w, `{"playerProfile": "`+string(marhshalleDplayerProfile)+`"}`)
 }
 
 func (adapter StatisticsServiceRestAdapter) Run() {
 	mux := mux.NewRouter()
 
-	// mux.HandleFunc("/login", adapter.LoginHandler).Methods("POST")
+	mux.HandleFunc("/playerProfile/{userID:[a-zA-Z0-9]+}", adapter.GetPlayerProfileHandler).Methods("GET")
 
 	adapter.Logger.Println("Starting server on Port 8080")
 	log.Fatalf("Error: Server failed to start: %v", http.ListenAndServe(":8080", mux))
