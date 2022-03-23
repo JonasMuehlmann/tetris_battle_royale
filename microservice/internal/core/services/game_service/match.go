695067e562dc6b58a8ef5ded5a82d3598603a227
Charlie <charlie.oliver.wiegand@gmail.com>
2022-03-23 11:01:10 +0100

    chore: remove acceleration from match

--- microservice/internal/core/services/game_service/match.go
+++ microservice/internal/core/services/game_service/match.go
@@ -1,22 +1,12 @@
 package gameService
 
-import "time"
-
 const MatchSize = 2
 
 type Match struct {
-	ID           string
-	Players      [MatchSize]Player
-	Acceleration float32
+	ID      string
+	Players [MatchSize]Player
 }
 
 func (match Match) Start() {
-	go match.setAcceleration()
-}
 
-func (match Match) setAcceleration() {
-	ticker := time.NewTicker(30 * time.Second)
-	for _ = range ticker.C {
-		match.Acceleration += 0.1
-	}
 }
