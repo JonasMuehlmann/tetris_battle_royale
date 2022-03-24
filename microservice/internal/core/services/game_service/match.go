9a9ebb9aa116981c478f1c32ee4f600844dfb3d8
jonas <jonasmuehlmann@protonmail.com>
2022-03-24 18:58:45 +0100

    feat(game_service): implement sending end of match data when game end

--- microservice/internal/core/services/game_service/match.go
+++ microservice/internal/core/services/game_service/match.go
@@ -1,5 +1,7 @@
 package gameService
 
+import "microservice/internal/core/types"
+
 const MatchSize = 2
 
 type Match struct {
@@ -10,6 +12,7 @@ type Match struct {
 	PlayerCount        int
 	NumPlayersAlive    int
 	EliminatedPlayers  map[string]bool
+	AlivePlayers       map[string]bool
 	GameService        *GameService
 	GameStop           chan bool
 }
@@ -18,7 +21,16 @@ func (match Match) Start() {
 }
 
 func (match *Match) Stop() {
-	// TODO: Handle End of match tasks like sending statistics, sending score board data, etc.
+	endOfMatchData, err := match.generateEndOfMatchData()
+	if err != nil {
+		match.GameService.Logger.Printf("Error: %v\n", err)
+	}
+
+	for _, player := range match.Players {
+
+		match.GameService.GameAdapter.SendEndOfMatchData(player.ID, endOfMatchData)
+	}
+
 }
 
 func (match *Match) HandlePlayerEliminations() {
@@ -28,10 +40,12 @@ func (match *Match) HandlePlayerEliminations() {
 		select {
 		case <-match.GameStop:
 			match.Stop()
+
 			return
 		case eliminatedPlayerID = <-match.PlayerEliminations:
 			match.NumPlayersAlive--
 			match.EliminatedPlayers[eliminatedPlayerID] = true
+			match.AlivePlayers[eliminatedPlayerID] = false
 
 			for _, player := range match.Players {
 				match.GameService.GameAdapter.SendEliminationNotice(player.ID, eliminatedPlayerID)
@@ -42,4 +56,22 @@ func (match *Match) HandlePlayerEliminations() {
 			}
 		}
 	}
+}
+
+func (match *Match) generateEndOfMatchData() (types.EndOfMatchData, error) {
+	endOfMatchData := types.EndOfMatchData{}
+
+	// NOTE: This should only do one iteration, IDK any other way to retrieve the sole element of a map
+	for playerID := range match.AlivePlayers {
+		endOfMatchData.Scorboard.Winner = playerID
+
+		break
+	}
+
+	for _, player := range match.Players {
+		playerPerformance := types.PlayerPerformance{player.ID, player.Score}
+		endOfMatchData.Scorboard.PlayerPerformances = append(endOfMatchData.Scorboard.PlayerPerformances, playerPerformance)
+	}
+
+	return endOfMatchData, nil
 }
